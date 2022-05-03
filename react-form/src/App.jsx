eba044872fd9d754bd316b2c02eb6184db10b28a
amar0020 <amarkrsoni8989@gmail.com>
2022-05-04 00:36:13 +0530

    mobile os

---
+++ react-form/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <input type="text"  placeholder='Name'/>
+     <input type="text" placeholder='age' />
+     <input type="text" placeholder='address' />
+     
+    </div>
+  );
+}
+
+export default App;
